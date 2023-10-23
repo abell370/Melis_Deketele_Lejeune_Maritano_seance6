bb8ac4cca3f241c0dde821b526aae5b1c42e902c
abell370 <ituniverse.sandbox@gmail.com>
2023-10-23 10:50:38 +0200

    + comment

--- seance6_cas1/src/main/java/Authenticator.java
+++ seance6_cas1/src/main/java/Authenticator.java
@@ -7,6 +7,7 @@ public abstract class Authenticator {
     }
 
     public boolean authenticate(String username, String password){
+        // vérification que le mot de passe lié à l'username retourné par getLogin correspond au mot de passe encodé
         return Objects.equals(getPassword(getLogin(username)), password);
     }
     public abstract String getLogin(String username);
